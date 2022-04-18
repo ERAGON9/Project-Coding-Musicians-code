4191f63d2a6fcfcb4b777a3e70e6668b0e83dcd8
Lior <liorbarak99@gmail.com>
2022-04-18 16:55:54 +0300

    Question 2 and half of 3 try

--- Project_Coding_Musicians_2022/Main.c
+++ Project_Coding_Musicians_2022/Main.c
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
-
 
 #include "Musician.h" // include inside at the begining a call for "Instrument Tree.h"
-=======
+
 /*
 Students:   Lior Barak
 			Shalev Kedar 314714080
@@ -11,13 +9,13 @@ try new thing
 */
 
 
->>>>>>> 5fcfdbf42e3604e9979498bfb55e2de6d6cc6736
+
 
 #include "FunctionsT.h"
 #include "FunctionsM.h"
 #include "FunctionsC.h"
 
-<<<<<<< HEAD
+
 
 typedef struct
 {
@@ -48,32 +46,22 @@ typedef struct cilist // -+
 
 }CIList;
 
-typedef struct
-=======
+
 void main()
->>>>>>> 5fcfdbf42e3604e9979498bfb55e2de6d6cc6736
 {
     FILE* ptr;
     InstrumentTree instruments;
     int instCount;
     ptr = fopen("TestyMctestface.txt", "r");
 
-    if (ptr == NULL) {
+    if (ptr == NULL) 
+	{
         printf("file can't be opened \n");
     }
 
-<<<<<<< HEAD
- 
-
-void main()
-{
-	
-
-	
-=======
     instruments = buildInstrumentsTree(ptr, &instCount);
 
     // freeAll();
 	fclose(ptr);
->>>>>>> 5fcfdbf42e3604e9979498bfb55e2de6d6cc6736
+
 }
